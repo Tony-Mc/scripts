d9fd40141604d5e1826f4b0799c30f6a429553c1
Tony <Tony-Mc@users.noreply.github.com>
2019-07-03 00:31:06 +0300

    Updated git-clean to check for git before executing

--- git-clean.sh
+++ git-clean.sh
@@ -1,19 +1,28 @@
-current="\*"
-master="master"
+source "utils/exists.sh"
 
-echo "Looking for local merged branches..."
-filtered=$(git branch --merged | egrep -v "(^$current|$master)")
+found=$(commandExists "git")
 
-if [ -z "$filtered" ]
-then
-    echo "Did not find any local merged branches."
+if [ "$found" -eq 0 ]; then
+    echo "git is not available"
+    exit
 else
-    echo "Cleaning local merged branches: $filtered"
-    git branch -d $filtered
+    current="\*"
+    master="master"
+
+    echo "Looking for local merged branches..."
+    filtered=$(git branch --merged | egrep -v "(^$current|$master)")
+
+    if [ -z "$filtered" ]
+    then
+        echo "Did not find any local merged branches."
+    else
+        echo "Cleaning local merged branches: $filtered"
+        git branch -d $filtered
+    fi
+
+    echo "Done"
+
+    echo "Pruning remote branches"
+    git fetch --prune
+    echo "Done"
 fi
-
-echo "Done"
-
-echo "Pruning remote branches"
-git fetch --prune
-echo "Done"
